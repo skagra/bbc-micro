fe513ad38ba9943cea886fad1fe71dc4832fd9be
Paul Harvey <Paul Harvey>
2021-04-23 19:35:06 +0100

    End of day

---
+++ BBCMicro/Asm/HelloName.asm
@@ -0,0 +1,31 @@
+﻿
+
+start:		PHA
+			PHA
+			JSR			print
+			BRK
+
+print:		LDX			#$0
+loop:		LDA			hello,X
+			BEQ			end
+			INX
+			JSR			OSWRCH
+			JMP			loop
+			RTS
+
+hello:		DC			"Hello World"
+			BYTE		0
+
+			Param  DW    0      ; Somewhere in ZP
+
+       LDA   #<data1
+       STA   Param
+       LDA   #>data1
+       STA   Param+1
+       JSR   Function
+       ...
+       LDA   #<data2
+       STA   Param
+       LDA   #>data2
+       STA   Param+1
+       JSR   Function
